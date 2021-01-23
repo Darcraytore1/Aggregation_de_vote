671d18c34035b4dc6e11b9727773c3918fe257dc
Darcraytore1 <57347517+Darcraytore1@users.noreply.github.com>
2021-01-23 12:03:06 +0100

    Start of angular UI

---
+++ API/condorcet_method.php
@@ -0,0 +1,30 @@
+<?php
+
+$json = file_get_contents("format.json");
+$parsed_json = json_decode($json);
+
+$tableauResult = array();
+
+// Le nombre de choix possibles 
+
+$count = count($parsed_json[0]);
+
+// Methode Condorcet
+
+// A contre B 
+// On pacourt le tableau pour récupérer combien de fois on été faite chaque composition de vote
+$i = 0;
+
+foreach ($parsed_json as $vote){
+    $array = array();
+    foreach ($vote as $choix){
+        array_push($array,$choix->choice);
+    }
+    $tableauResult[$i]['composition'] = $array;
+    $tableauResult[$i]['voteNbr'] = 1;
+    $i ++;
+}
+
+var_dump($tableauResult);
+
+?>
